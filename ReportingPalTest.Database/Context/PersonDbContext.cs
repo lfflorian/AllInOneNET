d817e7e7c0bd069897aa82ad4b65082a1f372876
lfflorian <lfflorian4@gmail.com>
2021-10-15 08:28:40 -0600

    entities and dbcontext definition

---
+++ ReportingPalTest.Database/Context/PersonDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReportingPalTest.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportingPalTest.Database.Context
+{
+    public class PersonDbContext : DbContext
+    {
+        public PersonDbContext(DbContextOptions<PersonDbContext> options) : base(options)
+        {}
+
+        public virtual DbSet<Person> People { get; set; }
+    }
+}
