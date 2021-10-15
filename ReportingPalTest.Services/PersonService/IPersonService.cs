9e1c109bdc7147b7b1844e905edcbb9d432b07c7
lfflorian <lfflorian4@gmail.com>
2021-10-15 08:35:11 -0600

    Definition of services

---
+++ ReportingPalTest.Services/PersonService/IPersonService.cs
@@ -0,0 +1,16 @@
+﻿using ReportingPalTest.Database.Context;
+using ReportingPalTest.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportingPalTest.Services.PersonService
+{
+    interface IPersonService
+    {
+        PersonDbContext DbContext { get; set; }
+        Task<Person> Get(int? id); 
+    }
+}
---
+++ ReportingPalTest.Services/PersonService/PersonService.cs
@@ -0,0 +1,21 @@
+﻿using ReportingPalTest.Database.Context;
+using ReportingPalTest.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportingPalTest.Services.PersonService
+{
+    class PersonService : IPersonService
+    {
+        public PersonDbContext DbContext { get; set; }
+
+        public async Task<Person> Get(int? id)
+        {
+            return await DbContext.People.Where(person => person.Id == id).FirstOrDefaultAsync();
+        }
+    }
+}
